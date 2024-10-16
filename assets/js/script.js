41361dd6c5397c7b6f140008e7ec48151efc5020
febri <82074350+febri-dot@users.noreply.github.com>
2024-10-17 01:03:11 +0700

    fixing multi language

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
+const BASE_URL = window.location.origin
+
 // Update Content On Window Loaded 
 window.addEventListener("DOMContentLoaded", async() => {
    const LANGUAGE = localStorage.getItem("language") || "idn";
 
    const LANGUAGE_DATA = await fetchLanguageData(LANGUAGE);
    updateLanguageContent(LANGUAGE_DATA);
+
+   // Active Language
+   document.querySelectorAll('[data-language]').forEach(element => {
+      lang = element.getAttribute("data-language");
+      
+      if(lang == LANGUAGE){
+         element.classList.add("active")
+      }
+   })
+   
 })
 
 
 // fetch language data from json file 
 async function fetchLanguageData(language) {
-   const RESPONSE = await fetch(`assets/languages/${language}.json`);
+   const RESPONSE = await fetch(`${BASE_URL}/assets/languages/${language}.json`);
    return RESPONSE.json()
 }
 
@@ -20,4 +32,10 @@ function updateLanguageContent(language){
       element.textContent = language[key];
    });
    
+}
+
+// Change Language 
+function changeLanguage(language){
+   localStorage.setItem('language', language);
+   location.reload();
 }
